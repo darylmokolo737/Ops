95156f4323b918361e1486ba02552743f6d3b764
darylmokolo737 <ubuntu@ip-172-31-91-151.ec2.internal>
2024-01-10 10:17:35 -0800

    first commit from linux1

---
+++ MyTemplate.py
@@ -0,0 +1,24 @@
+#!/usr/bin/python3
+### <DESCRIPTION OF SCRIPT>
+### <DM-1102024>
+# Set up initial variables and imports
+###< GLOBAL, INITIAL VARIABLES, AND IMPORTS (e.g. import sys)>
+# Main routine that is called when script is run
+def main():
+"""WHAT DOES THIS FUNCTION DO"""
+<
+For each function, put in a doc string that tells me what the function is
+suppose to do. For each block of code such as a loop, large if statement,
+etc, put a single line comment describing why it is needed
+>
+# Get the messages and process them
+<
+The functions code goes here. Use comments in the code to explain what
+large blocks of it do or for something you may not remember how it works
+>
+def sub1(message):
+"""WHAT DOES THIS FUNCTION DO"""
+# Run main() if script called directly, else use as a library to be imported
+if __name__ == "__main__":
+main()
+
--- README.md
+++ README.md
@@ -1,2 +1,4 @@
 # DM
+
+
 SEC444 Class scripts
