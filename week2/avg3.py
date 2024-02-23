735284e24a9a484e3ed9d8defa5cb03bc10cc20f
darylmokolo737 <ubuntu@ip-172-31-91-151.ec2.internal>
2024-02-23 12:57:34 -0800

    week2 updates

--- week2/avg3.py
+++ week2/avg3.py
@@ -1,10 +1,6 @@
 #!/usr/bin/python3
 ### <THIS SCRIPT CALCULATES THE AVERAGE OF THREE POSITIVE NUMBERS>
 ### <DM-1152024>
-
-
-
-
 #!/usr/bin/env python3
 
 import sys
@@ -16,13 +12,11 @@ def calculate_average(numbers):
     average = total / len(numbers)
     return round(average, 2)
 
-
-
 def main():
     # here I Check if the correct number of arguments is provided
     if len(sys.argv) != 4:
-       print("Usage: ./avg3.py <number1> <number2> <number3>")
-       sys.exit(1)
+        print("Usage: ./avg3.py <number1> <number2> <number3>")
+        sys.exit(1)
   
     # Check if all arguments are positive numbers
     try:
@@ -30,12 +24,12 @@ def main():
         if any(num < 0 for num in numbers):
             raise ValueError
     except ValueError:
-                                                                                                          print("Error: All arguments must be positive numbers.")
-                                                                                                          sys.exit(1)
+        print("Error: All arguments must be positive numbers.")
+        sys.exit(1)
 
-                                                                                                        # Calculate and print the average
-                                                                                                          average = calculate_average(numbers)
-                                                                                                          print("The average of the numbers is:", average)
+    # Calculate and print the average
+    average = calculate_average(numbers)
+    print("The average of the numbers is:", average)
 
                                                                                                 
 if __name__ == "__main__":                                                                            main()
